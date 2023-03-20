44c3130489f24370fc4e92f55d755c2e239930e1
BereniceArzate <arzate2323@outlook.com>
2023-03-20 12:08:14 -0600

    Imprimir histograma y valor rms

--- main.c
+++ main.c
@@ -15,14 +15,15 @@
  *	-[x] Imprime el valor maximo
  *	-[x] Imprime el valor Medio
  *	-[x] Imprime la mediana
- *	-[] Histograma
- *	-[] Valor RMS
+ *	-[x] Histograma
+ *	-[x] Valor RMS
  *	*/
 
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<math.h>
 
 #define ADC0 "/sys/bus/iio/devices/iio:device0/in_voltage0_raw"
 
@@ -34,7 +35,7 @@ int main(int argc, char* argv[]){
 	//fgets(reads,20,fp);		//Lee lo que esta en el puntero y guarda en reads
 	//printf("%s", reads);
 	int aux = 0;
-	int i = 0 ;
+	int i, j = 0 ;
 	int n = (int)strtol(argv[1],NULL,10);
 	int  arreglo[n];
 	float sum = 0.00;
@@ -43,7 +44,9 @@ int main(int argc, char* argv[]){
 	int vmin;
 	float med = 0.00;
 	float vmedian  = 0.00;
-	
+	float suma = 0.00;
+	float cuad = 0.00;
+	int maux[n];
 	if(argc!=2){
 		printf("Argumentos: %d \n", argc);
 		printf("Comando usado: adc-meas n \n");
@@ -57,8 +60,14 @@ int main(int argc, char* argv[]){
 		arreglo[i]=valor;
 		printf("%s \n", reads);
 		sum+=arreglo[i];
+		cuad = 	pow(arreglo[i], 2);
+		suma+=cuad;
+		maux[i] = 1;		
 		sleep(0.5);
 	}
+	float division = suma / n;
+	float rms = sqrt(division);
+	
 	vmax = arreglo[0];
 	vmin = arreglo[0];
 
@@ -73,7 +82,7 @@ int main(int argc, char* argv[]){
 	vmedia = sum / n;
 
     for (i=0; i< n; i++) {
-		for (int j=0; j<n-i-1; j++){
+		for (j=0; j<n-i-1; j++){
 			if(arreglo[j] > arreglo[j+1]){
 				aux  = arreglo[j+1];
 				arreglo[j+1] = arreglo[j];
@@ -84,6 +93,20 @@ int main(int argc, char* argv[]){
 	printf("Arreglo ordenado \n");
 	for (i=0; i<n; i++){
 		printf("%i \n", arreglo[i]);
+	}
+	
+	for(i=0;i<n-1;i++){
+		if (arreglo[i] == arreglo[i+1]){
+			maux[i]++;
+			maux[i+1] = 0;
+		}
+		for(j=0;j<i;j++){
+			if(arreglo[i] == arreglo[j]){
+				maux[i]+=maux[j];
+				maux[j] = 0;
+			}
+		}
+		
 	}
 
 	if (n%2 == 0){
@@ -97,8 +120,19 @@ int main(int argc, char* argv[]){
 	printf("El valor máximo es %i \n", vmax);
 	printf("El valor medio es %.2f \n", vmedia);
 	printf("La mediana es %.2f \n", vmedian);
+	printf("El valor RMS es %.2f \n", rms);
+	
+	for(i=0;i<n;i++){
+		if(maux[i]!=0){
+			printf("[%i]  ", arreglo[i]);
+			for(j=0;j<maux[i];j++){
+				printf("*");
+			}
+		printf("\n");
+		}
+	}
 
-
+	
     printf("OK \n");	
 	return 0;
 }
